e969e1c4291c6b0dc3da2a4e3bf3d5fe6f2e5f48
noachianark <noachianark@gmail.com>
2013-09-02 21:43:11 +0800

    添加工程文件。

---
+++ yaobao/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,116 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins\\org.apache.cordova.core.vibration\\www\\vibration.js",
+        "id": "org.apache.cordova.core.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.dialogs\\www\\notification.js",
+        "id": "org.apache.cordova.core.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.dialogs\\www\\android\\notification.js",
+        "id": "org.apache.cordova.core.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraConstants.js",
+        "id": "org.apache.cordova.core.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraPopoverOptions.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\Camera.js",
+        "id": "org.apache.cordova.core.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.camera\\www\\CameraPopoverHandle.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.device-orientation\\www\\CompassError.js",
+        "id": "org.apache.cordova.core.device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.device-orientation\\www\\CompassHeading.js",
+        "id": "org.apache.cordova.core.device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.device-orientation\\www\\compass.js",
+        "id": "org.apache.cordova.core.device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.geolocation\\www\\Coordinates.js",
+        "id": "org.apache.cordova.core.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.geolocation\\www\\PositionError.js",
+        "id": "org.apache.cordova.core.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.geolocation\\www\\Position.js",
+        "id": "org.apache.cordova.core.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.geolocation\\www\\geolocation.js",
+        "id": "org.apache.cordova.core.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.device-motion\\www\\Acceleration.js",
+        "id": "org.apache.cordova.core.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.device-motion\\www\\accelerometer.js",
+        "id": "org.apache.cordova.core.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    }
+]
+});
